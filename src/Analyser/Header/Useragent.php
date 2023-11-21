2df9c354923b1e576f6936129e5a4d2f7182b8c1
Maciej Koprek <maciej.koprek@fundrbird.com>
2023-11-21 12:44:09 +0100

    Fix WhichBrowser-ParserPHP deprecations logs

--- src/Analyser/Header/Useragent.php
+++ src/Analyser/Header/Useragent.php
@@ -6,6 +6,8 @@ class Useragent
 {
     use Useragent\Os, Useragent\Device, Useragent\Browser, Useragent\Application, Useragent\Using, Useragent\Engine, Useragent\Bot;
 
+    private $data;
+
     public function __construct($header, &$data, &$options)
     {
         $this->data =& $data;
